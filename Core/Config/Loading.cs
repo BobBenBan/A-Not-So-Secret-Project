ad5aad6251c6223c4a57a154b5ee9bf8e3ce6c75
Benjamin Ye <anemailofben@gmail.com>
2019-05-10 16:59:23 -0700

    Move Config folder/namespace

--- Core/Config/Loading.cs
+++
@@ -1,8 +0,0 @@
-namespace MusicMachine.Config
-{
-public static class Loading
-{
-    public static readonly string[] MeshDirs = {"res://Resources/Meshes"};
-    public static readonly string[] ShaderDirs = {"res://Resources/Shaders"};
-}
-}
